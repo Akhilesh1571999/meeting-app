623c488befef86133a26708497fa7c3be36f00da
Akhilesh <akhileshgargatte@gmail.com>
2023-11-30 07:32:56 +0530

    meeting-room-scheduler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {View , EventSettingsModel }from '@syncfusion/ej2-angular-schedule';
+// import {DataManager ,WebApiAdapter} from '@syncfusion/ej2-angular-schedule'; 
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:'<ejs-schedule height="850" width ="1250" [eventSettings]="eventObject" [currentView]="setView"></ejs-schedule>',
+  // templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-scheduler-app';
+  public setView: View ='WorkWeek';
+  // public setDate: Date=new Date (2023,10,15);
+  // private eventData:DataManager= new DataManager({
+  //   url: 'https://js.syncfusion.com/demos/ejservices/api/Schedule/loadData',
+  //   adaptor:new WebApiAdapter,
+  //   crossDomain:true
+  // });
+
+  public eventObject:EventSettingsModel ={
+    dataSource:[{
+      EventTitle:"",
+      EventStart: new Date(2023,10,17,4,0),
+      EventEnd: new Date(2023,10,17,6,0),
+      // IsReadonly: true
+      // RecurrenceRule:"FREQ=DAILY;INTERVAL=1;COUNT=10" 
+      // IsAllDay: true
+    }],
+    fields:{
+      subject:{name: 'EventTitle',default:"Hello",title:"Enter Title"}, 
+      startTime:{name: 'EventStart'},
+      endTime:{name: 'EventEnd'},
+    }
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScheduleModule ,RecurrenceEditorModule  ,DayService,WorkWeekService, TimelineViewsService, MonthService, MonthAgendaService } from '@syncfusion/ej2-angular-schedule';
+import { UserComponent } from './user/user.component';
+import { BookingComponent } from './booking/booking.component'
+
+// import { DataManager ,WebApiAdaptor} from '@syncfusion/ej2-data';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ScheduleModule,
+    RecurrenceEditorModule,
+    // DataManager,WebApiAdaptor
   ],
-  providers: [
+  providers: [DayService,WorkWeekService,TimelineViewsService ,MonthService ,MonthAgendaService,
     provideClientHydration()
   ],
   bootstrap: [AppComponent]
